88ebf4ec20468d9cc7f1c15037cf568eee9ff75b
veeksha45 <veeksha45@gmail.com>
2025-08-05 09:03:56 +0530

    using brevo

---
+++ utils/brevoClient.js
@@ -0,0 +1,8 @@
+// utils/brevoClient.js
+const SibApiV3Sdk = require('sib-api-v3-sdk');
+
+const defaultClient = SibApiV3Sdk.ApiClient.instance;
+const apiKey = defaultClient.authentications['api-key'];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+module.exports = new SibApiV3Sdk.TransactionalEmailsApi();
